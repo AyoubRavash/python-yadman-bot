3a3d94191366750e61d85d393686d261498f5b3d
Ayoub Ravash <ayoubravash@gmail.com>
2025-08-06 10:37:36 +0330

    changing user model and debug user service

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class User:
-    def __init__(self, telegram_id: int, first_name: str, last_name: str, username: str, joined_at: datetime = datetime.now(), birth_date: datetime = None, id: int = None):
+    def __init__(self, telegram_id: int, first_name: str, last_name: str, username: str, joined_at: datetime = datetime.now(), birth_date: date = None, id: int = None):
         self.id = id
         self.telegram_id = telegram_id
         self.first_name = first_name
--- services/user.py
+++ services/user.py
@@ -1,6 +1,7 @@
 from models.user import User
 from psycopg2 import connect
 from psycopg2.errors import UniqueViolation
+from datetime import datetime
 
 from config import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASS
 
@@ -46,7 +47,7 @@ async def get_user_db(telegram_id: int) -> User | None:
                 )
                 result = cur.fetchone()
                 if result[0] is not None:
-                    return User(result[1], result[2], result[3], result[4], result[5], id=result[0])
+                    return User(id=result[0], telegram_id=result[1], first_name=result[2], last_name=result[3], username=result[4], birth_date=result[5], joined_at=result[6])
                 else:
                     return None
             conn.commit()
@@ -58,3 +59,32 @@ async def get_user_db(telegram_id: int) -> User | None:
 
     finally:
         conn.close()
+
+
+async def update_user_field(telegram_id: int, field: str, value: str) -> bool:
+    if field == 'firstname':
+        db_field = 'first_name'
+    elif field == 'lastname':
+        db_field = 'last_name'
+    elif field == 'birthdate':
+        db_field = 'birth_date'
+    else:
+        return False
+
+    try:
+        with connect(host=DB_HOST, database=DB_NAME, port=DB_PORT, user=DB_USER, password=DB_PASS) as conn:
+            with conn.cursor() as cur:
+                query = f"UPDATE users SET {db_field} = %s WHERE telegram_id = %s"
+                cur.execute(query, (value, telegram_id))
+                conn.commit()
+                return cur.rowcount > 0  # True if any row was updated
+
+    except Exception as e:
+        print("Error updating user field:", e)
+        return False
+
+    finally:
+        try:
+            conn.close()
+        except:
+            pass
